83d22d68eecc00d6e1a967c41386d46f8ec637bb
huangdingQN <huangding@qnvip.com>
2018-06-21 17:37:53 +0800

    添加高可用的服务注册中心

--- service-miya/src/main/java/com/servicemiya/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/servicemiya/ServiceMiyaApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
